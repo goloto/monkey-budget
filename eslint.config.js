2e3fbde18d4e8e12b0d05b295b49f8c4ea531fea
Mikhail Goloto <mikhail@goloto.dev>
2025-10-21 22:06:31 +0300

    added personal eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import personalConfig from '@goloto/eslint-config';
+import jsEslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      personalConfig,
+      jsEslint.configs.recommended, 
+      tsEslint.configs.strict,
+      tsEslint.configs.stylistic,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    files: ['**/*.{ts,tsx}'],
   },
 ])
